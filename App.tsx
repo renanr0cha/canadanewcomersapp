69c97d2ec1ba41cef5ae4848b5ac852be77e97b7
Renan Rocha <renan.aragorn@gmail.com>
2023-07-16 17:56:35 -0300

    added the project's font

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import 'react-native-gesture-handler';
 
+import { Barlow_400Regular, Barlow_700Bold, Barlow_900Black, useFonts } from '@expo-google-fonts/barlow';
+import { ActivityIndicator } from 'react-native';
 import Routes from "./src/routes";
+
+const [ fontsLoaded ] = useFonts({ Barlow_400Regular, Barlow_700Bold, Barlow_900Black })
+
 
 export default function App() {
   return (
-    <Routes />
+    fontsLoaded ? <Routes /> : <ActivityIndicator size="large" color="#11455B" />
   );
 }
